03646a7c3bc26a6906e7efa7ad9bfbc8dcd6c74f
Wenbo Tao <thierryhenrytracy@163.com>
2014-10-18 17:36:12 +0800

    hw1 finished.

---
+++ hw1/hw1/README.md
@@ -0,0 +1,9 @@
+Draw a star using OpenGL
+=======
+
+1. Used glBegin(GL_LINE_LOOP) to draw a closed line which depicts a five-point star.
+
+2. Used glutKeyboardFunc(keyPressed) to catch the event of the keyboard.
+
+3. The source code is in hw1.cpp
+
--- hw1/hw1/hw1.cpp
+++ hw1/hw1/hw1.cpp
@@ -1,55 +1,62 @@
- #include "stdafx.h"
+#include "stdafx.h"
  
- #include<gl/glut.h>
+#include<gl/glut.h>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void mglVertex3f(float x, float y, float z)
+{
+	glVertex3f(x / 10, y / 10, z / 10);
+}
+
+void renderScene(void)
+{ 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
  
- //#include<gl/glu.h>　　//glut.h自动包含了glu.h 和 gl.h
+	glLoadIdentity();
  
- //#include<gl/gl.h>
+	glBegin(GL_LINE_LOOP);
+
+	mglVertex3f(1, 4, 0);
+	mglVertex3f(4, 4, 0);
+	mglVertex3f(5, 1, 0);
+	mglVertex3f(6, 4, 0);
+	mglVertex3f(9, 4, 0);
+	mglVertex3f(6.5, 6, 0);
+	mglVertex3f(7.5, 9, 0);
+	mglVertex3f(5, 7, 0);
+	mglVertex3f(2.5, 9, 0);
+	mglVertex3f(3.5, 6, 0);
+
+	glEnd();
+	glutSwapBuffers();
+
+}
+
+void keyPressed (unsigned char key, int x, int y)
+{
+	if (key == 27)
+		exit(0);
+}
+
+int _tmain(int argc, _TCHAR* argv[]) 
+{
  
-  
+	glutInit(&argc, (char**) argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
  
- void renderScene(void)
+	glutInitWindowPosition(100,100);
  
- {
+	glutInitWindowSize(320,320);
  
-          glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutCreateWindow("Hello OpenGL");
+
+	glutKeyboardFunc(keyPressed);
  
-          glLoadIdentity();
+	glutDisplayFunc(renderScene);
  
-          glBegin(GL_TRIANGLES);
+	glutMainLoop();
  
-          glVertex3f(-0.5,-0.5,0.0);
- 
-          glVertex3f(0.5,0.0,0.0);
- 
-          glVertex3f(0.0,0.5,0.0);
- 
-          glEnd();
- 
-          glutSwapBuffers();
- 
- }
- 
-  
- 
- int _tmain(int argc, _TCHAR* argv[])
- 
- {
- 
-          glutInit(&argc, (char**) argv);
- 
-          glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
- 
-          glutInitWindowPosition(100,100);
- 
-          glutInitWindowSize(320,320);
- 
-          glutCreateWindow("Hello OpenGL");
- 
-          glutDisplayFunc(renderScene);
- 
-          glutMainLoop();//enters the GLUT event processing loop.
- 
-          return 0;
- 
- }
+	return 0;
+}
