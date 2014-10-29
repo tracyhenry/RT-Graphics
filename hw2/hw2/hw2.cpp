1dcc32f039799ce5f664a11b7027ae3cec3c9039
Wenbo Tao <thierryhenrytracy@163.com>
2014-10-29 19:13:43 +0800

    A rotating TEAPOT!!!

--- hw2/hw2/hw2.cpp
+++ hw2/hw2/hw2.cpp
@@ -4,12 +4,38 @@
 #include "stdafx.h"
 #include<gl/glut.h>
 
+GLfloat material_ambient[] = {0.25f, 0.25f, 0.25f};
+GLfloat material_diffuse[] = {0.90f, 0.90f, 0.90f};
+GLfloat material_specular[] = {0.90f, 0.90f, 0.90f};
+GLfloat material_shininess = 25.0f;
+GLfloat angle;
+
+void Init()
+{
+	glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, material_shininess);
+
+	gluLookAt(0.0, 0.0, 5.0, 
+              0.0, 0.0, 0.0, 
+              0.0, 1.0, 0.0);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
+}
+
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glColor3f(0.0f, 1.0f, 0.0f);
-	glutWireTeapot(1.05f);
+
+	glPushMatrix();
+	glRotatef(angle, 1.0f, 1.0f, 0.0f);
+	glutSolidTeapot(1.05f);
+	glPopMatrix();
 	
 	glutSwapBuffers();
 }
@@ -17,12 +43,9 @@ void display(void)
 void reshape(int width, int height)
 {
 	glViewport(0, 0, width, height);
-
 	glMatrixMode(GL_PROJECTION);
-
 	glLoadIdentity();
 	gluPerspective(45.0, (double)width/(double)height, 1.0, 100.0);
-
 	glMatrixMode(GL_MODELVIEW);
 }
 
@@ -31,17 +54,17 @@ void keyPressed (unsigned char key, int x, int y)
 
 }
 
-void Init()
+void timer(int value)
 {
-	gluLookAt(0.0, 0.0, 5.0, 
-              0.0, 0.0, 0.0, 
-              0.0, 1.0, 0.0);
+	angle ++;
+	glutPostRedisplay();
+	glutTimerFunc(10, timer, 0);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	glutInit(&argc, (char**) argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(640,480);
 	glutCreateWindow("Primitive Transformation");
@@ -49,6 +72,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyPressed);
+	glutTimerFunc(10, timer, 0);
 
 	Init();
 
