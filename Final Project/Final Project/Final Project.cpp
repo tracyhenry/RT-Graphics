14fde0f3cf5f503ff5b0a7c1f9bf59475968000e
Wenbo Tao <thierryhenrytracy@163.com>
2015-01-15 16:15:53 +0800

    Drawed two cubes & installed proper light sources.

--- Final Project/Final Project/Final Project.cpp
+++ Final Project/Final Project/Final Project.cpp
@@ -9,16 +9,17 @@
 using namespace std;
 
 //Real numbers to determine the materials
-GLfloat material_ambient[] = {0.25f, 0.25f, 0.25f};
+GLfloat material_ambient[] = {0.5f, 0.5f, 0.5f};
 GLfloat material_diffuse[] = {0.10f, 0.10f, 0.10f};
 GLfloat material_specular[] = {0.40f, 0.40f, 0.40f};
-GLfloat material_shininess = 25.0f;
+GLfloat material_shininess = 50.0f;
 
 GLfloat cYellow[] = {1.0, 1.0, 0.2, 1.0};
 GLfloat cPeach[] = {1.0, 0.937, 0.83};
-GLfloat cGreen[] = {0.0, 1.0, 0.0, 1.0}; //Green Color
-GLfloat cWhite[] = {1.0, 1.0, 1.0, 1.0}; //White Color
+GLfloat cGreen[] = {0.0, 1.0, 0.0, 1.0};
+GLfloat cWhite[] = {1.0, 1.0, 1.0, 1.0};
 GLfloat cBlue[] = {0.0, 0.0, 1.0, 1.0};
+GLfloat cGray[] = {0.6, 0.6, 0.6, 1.0};
 GLfloat cRed[] = {1.0, 0.0, 0.0, 1.0};
 
 
@@ -34,13 +35,36 @@ void Init()
 	glMaterialf(GL_FRONT, GL_SHININESS, material_shininess);
 
 	//Perspective Projection
-	gluLookAt(0.0, -50.0, 50.0, 
-              0.0, 0.0, 0.0, 
-              0.0, 1.0, 0.0);
+	gluLookAt(30, -50, 30, 
+              0, 0, 0, 
+              -3, 5, 0);
 
 	//Set light sources
+	GLfloat lightpos0[] = {100, 100, 0, 0};
+	
+	GLfloat light0_position[] = {-100, 0, 0, 0};
+	GLfloat light1_position[] = {100, 0, 0, 0};	
+	GLfloat light2_position[] = {0, -500, 0, 0};	
+	GLfloat light_colors[] = {1, 1, 1, 1};
+
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
+	glEnable(GL_LIGHT2);
+
+	glPushMatrix();
+	glTranslatef(0, 0, 0);
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+	glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+	glLightfv(GL_LIGHT2, GL_POSITION, light2_position);
+	glPopMatrix();
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_colors);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, light_colors);
+	glLightfv(GL_LIGHT2, GL_DIFFUSE, light_colors);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_colors);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, light_colors);
+	glLightfv(GL_LIGHT2, GL_SPECULAR, light_colors);
+	
 
 	//Enable depth
 	glEnable(GL_DEPTH_TEST);
@@ -74,11 +98,28 @@ void display(void)
 
 	//Draw a rectangle
 	glPushMatrix();
+	glTranslatef(0, 0, 1e-2);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, cGreen);
-	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 1.2);
-	glRectf(10.0f, 20.0f, -10.0f, -20.0f);	
+	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 1.2);	
+	glRectf(10, 20, -10, -20);
+	glPopMatrix(); 
+
+	//Draw two cubes
+	glPushMatrix();
+	glTranslatef(0, -10, -11);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, cGray);
+	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 1);
+	glutSolidCube(22);
 	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(0, 10, -11);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, cGray);
+	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 1);
+	glutSolidCube(22);
+	glPopMatrix();  
+
+	
 	glutSwapBuffers();
 }
 
